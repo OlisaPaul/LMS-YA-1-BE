6b9e400501f7677ff017524c12fd016a188ec9a6
OlisaPaul <odirahchukwumma28@gmail.com>
2023-05-27 23:55:06 +0100

    Fix - imported and used the cors middleware

--- src/startup/routes.startup.js
+++ src/startup/routes.startup.js
@@ -1,10 +1,12 @@
 // This file determines which of the routes will be used based on the api url
 const express = require("express");
+const cors = require('cors');
 const error = require("../middleware/error.middleware");
 const auth = require("../routes/auth.routes");
 const users = require("../routes/users.routes");
 
 module.exports = function (app) {
+  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
