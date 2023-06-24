e6f002c0051633c2735bc37499f6123f5e5fd385
OlisaPaul <odirahchukwumma28@gmail.com>
2023-06-24 16:10:09 +0100

    Feature - thumbnails: made the thumbnails to handle file upload

--- src/routes/thumbnails.routes.js
+++ src/routes/thumbnails.routes.js
@@ -1,21 +1,35 @@
+const express = require("express");
+const multer = require("multer");
 const validateMiddleware = require("../middleware/validate.middleware");
 const admin = require("../middleware/admin.middleware");
 const auth = require("../middleware/auth.middleware");
-const { validate, validatePatch } = require("../model/thumbnail.model");
-const express = require("express");
-const router = express.Router();
+const {
+  validate,
+  validatePatch,
+  imageSchema,
+} = require("../model/thumbnail.model");
 const asyncMiddleware = require("../middleware/async.middleware");
 const validateObjectId = require("../middleware/validateObjectId.middleware");
 const thumbnailController = require("../controllers/thumbnail.controllers");
 const validLearningtrackMiddleware = require("../middleware/validLearningtrack.middleware");
+const multerErrorMiddleware = require("../middleware/multerError.middleware");
+const validateFileMiddleware = require("../middleware/validateFile.middleware");
+const multerCommon = require("../common/multer.common");
+
+const router = express.Router();
+const fieldName = "image";
+const fileSize = 5;
+const upload = multer(multerCommon(multer, fileSize)).single(fieldName);
 
 // This is used for registering a new thumbnail.
 router.post(
   "/",
   auth,
   admin,
+  multerErrorMiddleware(upload, multer, fileSize, fieldName),
+  validateFileMiddleware("Image", imageSchema),
   validateMiddleware(validate),
-  asyncMiddleware(thumbnailController.addNewThumbnail)
+  asyncMiddleware(thumbnailController.uploadThumbnail)
 );
 
 router.get("/", asyncMiddleware(thumbnailController.fetchAllThumbnails));
